db3f7c7281a645cbef7001edb0758de32da29126
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2015-12-20 02:46:58 +0000

    deleted incomplete original version

--- Day4Ex3.groovy
+++
@@ -1,6 +0,0 @@
-print "enter total amount borrowed: "
-double amount = System.console().readLine()
-print "enter number of years to pay: "
-double years = System.console().readLine()
-print "enter interest rate"
-double rate = System.console().readLine()
